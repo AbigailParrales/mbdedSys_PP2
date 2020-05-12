d62301f7034926b601e69c0fa6d74c12d975af51
AbigailParrales <danielaparralesd.m@gmail.com>
2020-05-11 21:24:25 -0500

    Ading Doxyfile style documentation to src files

--- LCDFR/Core/Src/i2c-lcd.c
+++ LCDFR/Core/Src/i2c-lcd.c
@@ -31,6 +31,7 @@ extern I2C_HandleTypeDef hi2c1;  // change your handler here accordingly
 
 /**
  * Send LCD comand
+ * @param cmd
 */
 void lcd_send_cmd (char cmd)
 {
@@ -38,26 +39,33 @@ void lcd_send_cmd (char cmd)
 	uint8_t data_t[4];
 	data_u = (cmd&0xf0);
 	data_l = ((cmd<<4)&0xf0);
-	data_t[0] = data_u|0x0C;  //en=1, rs=0
-	data_t[1] = data_u|0x08;  //en=0, rs=0
-	data_t[2] = data_l|0x0C;  //en=1, rs=0
-	data_t[3] = data_l|0x08;  //en=0, rs=0
+	data_t[0] = data_u|0x0C;  /*!< en=1, rs=0 */
+	data_t[1] = data_u|0x08;  /*!< en=0, rs=0 */
+	data_t[2] = data_l|0x0C;  /*!< en=1, rs=0 */
+	data_t[3] = data_l|0x08;  /*!< en=0, rs=0 */
 	HAL_I2C_Master_Transmit (&hi2c1, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, 100);
 }
 
+/**
+ * Send data to display in the LCD
+ * @param data
+*/
 void lcd_send_data (char data)
 {
 	char data_u, data_l;
 	uint8_t data_t[4];
 	data_u = (data&0xf0);
 	data_l = ((data<<4)&0xf0);
-	data_t[0] = data_u|0x0D;  //en=1, rs=0
-	data_t[1] = data_u|0x09;  //en=0, rs=0
-	data_t[2] = data_l|0x0D;  //en=1, rs=0
-	data_t[3] = data_l|0x09;  //en=0, rs=0
+	data_t[0] = data_u|0x0D;  /* !< en=1, rs=0 */
+	data_t[1] = data_u|0x09;  /* !< en=0, rs=0 */
+	data_t[2] = data_l|0x0D;  /* !< en=1, rs=0 */
+	data_t[3] = data_l|0x09;  /* !< en=0, rs=0 */
 	HAL_I2C_Master_Transmit (&hi2c1, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, 100);
 }
 
+/**
+ * Clear LCD
+*/
 void lcd_clear (void)
 {
 	lcd_send_cmd (0x80);
@@ -67,6 +75,9 @@ void lcd_clear (void)
 	}
 }
 
+/**
+ * Initialize LCD
+*/
 void lcd_init (void)
 {
 	lcd_send_cmd (0x02);
@@ -75,6 +86,10 @@ void lcd_init (void)
 	lcd_send_cmd (0x80);
 }
 
+/**
+ * Print string in the LCD
+ * @param str
+*/
 void lcd_send_string (char *str)
 {
 	while (*str) lcd_send_data (*str++);
